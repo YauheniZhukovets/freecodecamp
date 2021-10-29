7e22b31f428854598314fd631a71b8480bc4cd0a
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2021-10-29 20:01:40 +0300

    Build JavaScript Objects

---
+++ 1_basic_JS/84BuildJavaScriptObjects.js
@@ -0,0 +1,10 @@
+const myDog = {
+    // Only change code below this line
+    name: "Graf",
+    legs: 4,
+    tails: 1,
+    friends: ["Zhukovets", "Yauheni"]
+  
+  
+    // Only change code above this line
+  };
