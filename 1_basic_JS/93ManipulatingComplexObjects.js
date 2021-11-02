0d65e261cdebcb041cb1bef7100c454e75f07e37
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2021-11-02 23:12:06 +0300

    Manipulating Complex Objects

---
+++ 1_basic_JS/93ManipulatingComplexObjects.js
@@ -0,0 +1,25 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+  
+    {  
+      "artist": "Stigmata",
+      "title": "October",
+      "release_year": 2007,
+       "formats": [
+        "MP3",
+        "CD",
+        "FLAC",
+        "AAC"
+      ]
+    }
+  ];
