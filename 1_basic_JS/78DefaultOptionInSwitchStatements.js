ffbd1442046623743974b3270b5484e83e167b6c
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-01-27 22:02:35 +0300

    78 new switch

--- 1_basic_JS/78DefaultOptionInSwitchStatements.js
+++ 1_basic_JS/78DefaultOptionInSwitchStatements.js
@@ -21,3 +21,32 @@ function switchOfStuff(val) {
   }
   
   switchOfStuff(1);
+//пример 2
+let browser = prompt("Каким браузером вы пользуетесь:")
+if (browser === 'Edge') {
+    alert("You've got the Edge!")
+} else if (browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera') {
+    alert('Okay we support these browsers too');
+} else {
+    alert('We hope that this page looks ok!');
+}
+
+
+
+//пример 3
+const number = +prompt('Введите число между 0 и 3')
+
+switch (number) {
+    case 0:
+        alert('Вы ввели число 0')
+        break
+    case 1:
+        alert('Вы ввели число 1')
+        break
+    case 2:
+    case 3:
+        alert('Вы ввели число 2, а может и 3')
+        break
+
+}
+
